71ed10f641f270daff9ba5749e9a0e736587c2a4
Alekasandr26 <a.brendin@mail.ru>
2021-09-29 11:59:56 +0400

    Create Component with functions that add or delet posts

---
+++ src/components/AddPost/AddPost.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import Button from "../../UI/Button/Button";
+import Input from "../../UI/Input/Input";
+import classes from "./AddPost.module.scss";
+import Post from "./Post/Post";
+import PostForm from "./PostForm/PostForm";
+
+const AddPost = () => {
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: "JavaScript",
+      body: "I learning Js and i will be frontend developer",
+    },
+    {
+      id: 2,
+      title: "React Js",
+      body: "I learning Js and React and i will be React frontend developer",
+    },
+    {
+      id: 3,
+      title: "TypeScript",
+      body: "I learning TypeScript and i will use TypeScript with Js and React",
+    },
+  ]);
+
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost]);
+  };
+
+  const removePost = (post) => {
+    setPosts(posts.filter((p) => p.id !== post.id));
+  };
+
+  return (
+    <>
+      <div className={classes.post}>
+        <div className={classes.post__wrapper}>
+          <h1 className={classes.post__title}>Add Post</h1>
+          <div className={classes.post__input__wrapper}>
+            <PostForm create={createPost} />
+          </div>
+        </div>
+      </div>
+      <div className={classes.post__posts}>
+        <div className={classes.post__sort}>
+          <h1>Posts: {posts.length}</h1>
+          <Button>Sort</Button>
+        </div>
+      </div>
+      <div className={classes.post__container}>
+        {posts.map((post, index) => {
+          return (
+            <Post
+              removePost={removePost}
+              number={index + 1}
+              post={post}
+              key={post.id}
+              title={post.title}
+              body={post.body}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default AddPost;
---
+++ src/components/AddPost/PostForm/PostForm.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import Button from "../../../UI/Button/Button";
+import Input from "../../../UI/Input/Input";
+import classes from "./PostForm.module.scss";
+
+const PostForm = ({create}) => {
+  const [post, setPost] = useState({ title: "", body: "" });
+  const addNewPost = (e) => {
+    e.preventDefault();
+    if (post.title === "" || post.body === "") {
+      return null;
+    }
+    const newPost = {
+      ...post,
+      id: Date.now(),
+    };
+    create(newPost)
+    setPost({ title: "", body: "" });
+  };
+
+  return (
+    <form>
+      <Input
+        type="text"
+        placeholder="Title post"
+        value={post.title}
+        onChange={(e) => setPost({ ...post, title: e.target.value })}
+      />
+      <Input
+        type="text"
+        placeholder="Text post"
+        value={post.body}
+        onChange={(e) => setPost({ ...post, body: e.target.value })}
+      />
+      <div className={classes.post__btns}>
+        {/*<Button>changel</Button>*/}
+        <Button type={"submit"} onClick={addNewPost}>
+          send post
+        </Button>
+      </div>
+    </form>
+  );
+};
+
+export default PostForm;
