07e25ac83ba6a9030517a7d074dbc01bd66974dd
Alekasandr26 <a.brendin@mail.ru>
2021-09-30 09:46:18 +0400

    add animation

---
+++ src/components/AddPost/PostList/PostFilter/PostFilter.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Input from "../../../../UI/Input/Input";
+import Select from "../../../../UI/Select/Select";
+import classes from './PostFilter.module.scss'
+
+const PostFilter = ({filter, setFilter}) => {
+  return (
+    <>
+      <Select
+        value={filter.sort}
+        onChange={selectedSort => setFilter({...filter, sort: selectedSort})}
+        defaultValue={"Sort by"}
+        options={[
+          { value: "title", name: "By name" },
+          { value: "body", name: "By description" },
+        ]}
+      />
+      <div className={classes.searh__input}>
+        <Input
+          value={filter.query}
+          onChange={(e) => setFilter({...filter, query: e.target.value})}
+          placeholder={"Search post"}
+        />
+      </div>
+    </>
+  );
+};
+
+export default PostFilter;
